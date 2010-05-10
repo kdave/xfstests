f73e0fb3f2c59c4b08542b71fcf6aa38e41987d1
Christoph Hellwig <hch@lst.de>
2010-05-10 19:39:04 +0200

    xfstests: test extent size hints
    This is an adapted version of the reproduced in OSS BZ #874, which
    shows file corruption when using the extent size hint.
    
    Signed-off-by: Geoffrey Wehrman <gwehrman@sgi.com>
    Signed-off-by: Christoph Hellwig <hch@lst.de>

---
+++ src/t_holes.c
@@ -0,0 +1,41 @@
+/*
+ * Copyright (c) 2010 Silicon Graphics, Inc.  All Rights Reserved.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it would be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write the Free Software Foundation,
+ * Inc.,  51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <stdio.h>
+
+int main(int argc, char **argv)
+{
+	int buf[1024];
+	int fd, i, j;
+
+	fd = open(argv[1], O_RDWR|O_CREAT, 0666);
+        for (i = 1; i < 9100; i++) {
+		for (j = 0; j < 1024; j++)
+			buf[j]  = i | i << 5;
+
+		if (write(fd,buf,253*4*sizeof(int))!= 253*4*sizeof(int))
+			printf("Write did not return correct amount\n");
+
+		if ((i % 9) == 0 && i < 9001)
+			lseek(fd, 4096 * 110,SEEK_CUR);
+	}
+
+	close(fd);
+	return 0;
+}
