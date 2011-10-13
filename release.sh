8baf6c07bd88dc826186222023a423c37ba46ef3
Alex Elder <aelder@sgi.com>
2011-10-13 16:31:04 +0000

    xfstests: prepare for marking an initial versioned release
    Add the release script used in the other XFS user space packages.
    The version is set to 1.1.0, to differentiate it from the 1.0.0
    version that was recorded in the VERSION file.
    
    Signed-off-by: Alex Elder <aelder@sgi.com>
    Reviewed-by: Christoph Hellwig <hch@lst.de>

---
+++ release.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#
+# Automate generation a new release
+#
+
+. ./VERSION
+
+version=${PKG_MAJOR}.${PKG_MINOR}.${PKG_REVISION}
+date=`date +"%-d %B %Y"`
+
+echo "Updating CHANGES"
+sed -e "s/${version}.*/${version} (${date})/" doc/CHANGES > doc/CHANGES.tmp && \
+	mv doc/CHANGES.tmp doc/CHANGES
+
+echo "Commiting CHANGES update to git"
+git commit -s -a -m "${version} release"
+
+echo "Tagging git repository"
+git tag -s -a -m "${version} release" v${version}
+
+echo "Creating source tarball"
+make dist
+
+echo "Done."
+echo "Please remember to push out tags using \"git push --tags\""
