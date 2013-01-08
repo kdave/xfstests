2388d7d861e551a63420a1982dd6661076729123
Andrew Dahl <adahl@sgi.com>
2013-01-08 12:23:08 -0600

    xfstests: SGI license update for looptest.c
    Updated SGI license in looptest.c 
    
    Signed-off-by: Andrew Dahl <adahl@sgi.com>
    Reviewed-by: Ben Myers <bpm@sgi.com>
    Signed-off-by: Ben Myers <bpm@sgi.com>

--- src/looptest.c
+++ src/looptest.c
@@ -1,29 +1,18 @@
 /*
- * Copyright  2002 Silicon Graphics, Inc.  ALL RIGHTS RESERVED
- * 
- *  U.S. GOVERNMENT RESTRICTED RIGHTS LEGEND
- * 
- *  Use, duplication or disclosure by the Government is subject to restrictions
- *  as set forth in FAR 52.227.19(c)(2) or subparagraph (c)(1)(ii) of the 
- *  Rights in Technical Data and Computer Software clause at DFARS 252.227-7013 
- *  and/or in similar or successor clauses in the FAR, or the DOD or NASA FAR
- *  Supplement. Unpublished -- rights reserved under the Copyright Laws 
- *  of the United States. Contractor/manufacturer is Silicon Graphics, Inc.,
- *  2011 N. Shoreline Blvd., Mountain View, CA 94039-7311.
- * 
- *  THIS IS UNPUBLISHED PROPRIETARY SOURCE CODE OF SGI
- * 
- *  The copyright notice above does not evidence any actual or intended 
- *  publication or disclosure of this source code, which includes information 
- *  that is the confidential and/or proprietary, and is a trade secret,
- *  of Silicon Graphics, Inc. Any use, duplication or disclosure not 
- *  specifically authorized in writing by Silicon Graphics is strictly 
- *  prohibited. ANY DUPLICATION, MODIFICATION, DISTRIBUTION,PUBLIC PERFORMANCE,
- *  OR PUBLIC DISPLAY OF THIS SOURCE CODE WITHOUT THE EXPRESS WRITTEN CONSENT 
- *  OF SILICON GRAPHICS, INC. IS STRICTLY PROHIBITED.  THE RECEIPT OR POSSESSION
- *  OF THIS SOURCE CODE AND/OR INFORMATION DOES NOT CONVEY ANY RIGHTS 
- *  TO REPRODUCE, DISCLOSE OR DISTRIBUTE ITS CONTENTS, OR TO MANUFACTURE, USE, 
- *  OR SELL ANYTHING THAT IT MAY DESCRIBE, IN WHOLE OR IN PART.
+ * Copyright (c) 2013 SGI.  All Rights Reserved.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it would be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write the Free Software Foundation,
+ * Inc.,  51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
  */
  
 /* dxm - 28/2/2 */
