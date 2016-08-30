0dea6dc3eaba902347d0fefd249bbe16b493ae4f
Artem Savkov <asavkov@redhat.com>
2016-08-30 09:39:07 +0200

    generic: listxattr syscall with different buffer sizes
    Add test that calls listxattr syscall with different buffer size
    arguments checking if it fails properly.
    
    Signed-off-by: Artem Savkov <asavkov@redhat.com>
    Reviewed-by: Eryu Guan <eguan@redhat.com>
    Signed-off-by: Eryu Guan <eguan@redhat.com>

---
+++ src/listxattr.c
@@ -0,0 +1,73 @@
+/*
+ * Copyright (c) 2016 Red Hat, Inc.  All Rights Reserved.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, either version 2 of
+ * the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it would be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <errno.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/xattr.h>
+
+int main(int argc, char **argv)
+{
+        int ret;
+        size_t bufsize = 0;
+        char *buf = NULL;
+
+        if (argc < 2) {
+                fprintf(stderr, "usage: %s <testfile> [bufsize]\n", argv[0]);
+                return 1;
+        };
+
+        if (argc > 2) {
+                bufsize = strtoul(argv[2], NULL, 10);
+                if (bufsize == -1) {
+                        perror("buffsize");
+                        return 1;
+                }
+        }
+
+        if (bufsize == 0) {
+                bufsize = listxattr(argv[1], NULL, 0);
+                if (bufsize == -1) {
+                        perror("listxattr");
+                        return 1;
+                }
+        }
+
+        buf = malloc(bufsize);
+        if (buf == NULL) {
+                perror("buf alloc");
+                return 1;
+        }
+
+        ret = listxattr(argv[1], buf, bufsize);
+        if (ret < 0) {
+                perror("listxattr");
+        } else {
+                char *l;
+                for (l = buf; l != (buf + bufsize) && *l != '\0';
+                                l = strchr(l, '\0') + 1) {
+                        printf("xattr: %s\n", l);
+                }
+        }
+
+        free(buf);
+
+        return 0;
+}
