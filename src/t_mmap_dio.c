kdave/xfstests

commit 456581661b4d34690b508e74a06c4922ed63390f

xfs: test per-inode DAX flag by IO
In a DAX mountpoint, do IO betwen files with and
without DAX per-inode flag. We do mmap, both
O_DIRECT and buffered read/write IO in this case.
Then test again in the same device without dax
mountoption.

Add help _require_scratch_dax to make sure we can
test DAX feature on SCRATCH_DEV.

Add mmap dio test programme to test read/write
between a mmap area of one file and another file
directly or buffered, with different size.

Signed-off-by: Ross Zwisler <ross.zwisler@linux.intel.com>
Signed-off-by: Xiong Zhou <xzhou@redhat.com>
Reviewed-by: Eryu Guan <eguan@redhat.com>
Signed-off-by: Eryu Guan <eguan@redhat.com>

src/t_mmap_dio.c
@@ -1,0 +1,111 @@
+/*
+ * This programme was originally written by
+ *     Jeff Moyer <jmoyer@redhat.com>
+ *
+ * Copyright (C) 2016, Red Hat, Inc.
+ */
+#define _GNU_SOURCE 1
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/mman.h>
+#include <libaio.h>
+#include <errno.h>
+#include <sys/time.h>
+
+void usage(char *prog)
+{
+	fprintf(stderr,
+		"usage: %s <src file> <dest file> <size> <msg>\n",
+		prog);
+	exit(1);
+}
+
+void err_exit(char *op, unsigned long len, char *s)
+{
+	fprintf(stderr, "%s(%s) len %lu %s\n",
+		op, strerror(errno), len, s);
+	exit(1);
+}
+
+int main(int argc, char **argv)
+{
+	int fd, fd2, ret, dio = 1;
+	char *map;
+	char *msg;
+	char *sfile;
+	char *dfile;
+	unsigned long len, opt;
+
+	if (argc < 4)
+		usage(basename(argv[0]));
+
+	while ((opt = getopt(argc, argv, "b")) != -1)
+		dio = 0;
+
+	sfile = argv[optind];
+	dfile = argv[optind + 1];
+	msg = argv[optind + 3];
+	len = strtoul(argv[optind + 2], NULL, 10);
+	if (errno == ERANGE)
+		err_exit("strtoul", 0, msg);
+
+	/* Open source file and mmap*/
+	fd = open(sfile, O_RDWR, 0644);
+	if (fd < 0)
+		err_exit("open src", len, msg);
+
+	map = (char *)mmap(NULL, len,
+		PROT_READ|PROT_WRITE, MAP_SHARED, fd, 0);
+	if (map == MAP_FAILED)
+		err_exit("mmap", len, msg);
+
+	if (dio == 1) {
+		/* Open dest file with O_DIRECT */
+		fd2 = open(dfile, O_RDWR|O_DIRECT, 0644);
+		if (fd2 < 0)
+			err_exit("open dest", len, msg);
+	} else {
+		/* Open dest file without O_DIRECT */
+		fd2 = open(dfile, O_RDWR, 0644);
+		if (fd2 < 0)
+			err_exit("open dest", len, msg);
+	}
+
+	/* First, test storing to dest file from source mapping */
+	ret = write(fd2, map, len);
+	if (ret != len)
+		err_exit("write", len, msg);
+
+	ret = fsync(fd2);
+	if (ret != 0)
+		err_exit("fsync", len, msg);
+
+	ret = (int)lseek(fd2, 0, SEEK_SET);
+	if (ret == -1)
+		err_exit("lseek", len, msg);
+
+	/* Next, test reading from dest file into source mapping */
+	ret = read(fd2, map, len);
+	if (ret != len)
+		err_exit("read", len, msg);
+	ret = msync(map, len, MS_SYNC);
+	if (ret < 0)
+		err_exit("msync", len, msg);
+
+	ret = munmap(map, len);
+	if (ret < 0)
+		err_exit("munmap", len, msg);
+
+	ret = close(fd);
+	if (ret < 0)
+		err_exit("clsoe fd", len, msg);
+
+	ret = close(fd2);
+	if (ret < 0)
+		err_exit("close fd2", len, msg);
+
+	exit(0);
+}

.gitignore
@@ -129,6 +129,7 @@
 /src/cloner
 /src/renameat2
 /src/t_rename_overwrite
+/src/t_mmap_dio
 
 # dmapi/ binaries
 /dmapi/src/common/cmd/read_invis

common/rc
@@ -2677,6 +2677,19 @@ _require_scratch_shutdown()
 	_scratch_unmount
 }
 
+# Does dax mount option work on this dev/fs?
+_require_scratch_dax()
+{
+	_require_scratch
+	_scratch_mkfs > /dev/null 2>&1
+	_scratch_mount -o dax
+	# Check options to be sure. XFS ignores dax option
+	# and goes on if dev underneath does not support dax.
+	_fs_options $SCRATCH_DEV | grep -qw "dax" || \
+		_notrun "$SCRATCH_DEV $FSTYP does not support -o dax"
+	_scratch_unmount
+}
+
 # Does norecovery support by this fs?
 _require_norecovery()
 {

src/Makefile
@@ -12,7 +12,7 @@ TARGETS = dirstress fill fill2 getpagesize holes lstat64 \
 	godown resvtest writemod makeextents itrash rename \
 	multi_open_unlink dmiperf unwritten_sync genhashnames t_holes \
 	t_mmap_writev t_truncate_cmtime dirhash_collide t_rename_overwrite \
-	holetest t_truncate_self
+	holetest t_truncate_self t_mmap_dio
 
 LINUX_TARGETS = xfsctl bstat t_mtab getdevicesize preallo_rw_pattern_reader \
 	preallo_rw_pattern_writer ftrunc trunc fs_perms testx looptest \

tests/xfs/260
@@ -1,0 +1,158 @@
+#! /bin/bash
+# FS QA Test 260
+#
+# Test per-inode DAX flag by mmap direct/buffered IO.
+#
+#-----------------------------------------------------------------------
+# Copyright (c) 2017 Red Hat Inc.  All Rights Reserved.
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License as
+# published by the Free Software Foundation.
+#
+# This program is distributed in the hope that it would be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write the Free Software Foundation,
+# Inc.,  51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+#-----------------------------------------------------------------------
+#
+
+seq=`basename $0`
+seqres=$RESULT_DIR/$seq
+echo "QA output created by $seq"
+
+here=`pwd`
+tmp=/tmp/$$
+status=1	# failure is the default!
+trap "_cleanup; exit \$status" 0 1 2 3 15
+
+_cleanup()
+{
+	cd /
+	rm -f $tmp.*
+}
+
+# get standard environment, filters and checks
+. ./common/rc
+. ./common/filter
+
+# remove previous $seqres.full before test
+rm -f $seqres.full
+
+_supported_fs xfs
+_supported_os Linux
+_require_scratch_dax
+_require_test_program "feature"
+_require_test_program "t_mmap_dio"
+_require_xfs_io_command "chattr" "+/-x"
+_require_xfs_io_command "falloc"
+
+prep_files()
+{
+	rm -f $SCRATCH_MNT/tf_{s,d}
+
+	$XFS_IO_PROG -f -c "falloc 0 $tsize" \
+		$SCRATCH_MNT/tf_{s,d} >> $seqres.full 2>&1
+}
+
+t_both_dax()
+{
+	prep_files
+	$XFS_IO_PROG -c "chattr +x" $SCRATCH_MNT/tf_{s,d}
+	# with O_DIRECT first
+	src/t_mmap_dio $SCRATCH_MNT/tf_{s,d} $1 "dio both dax"
+
+	prep_files
+	$XFS_IO_PROG -c "chattr +x" $SCRATCH_MNT/tf_{s,d}
+	# again with buffered IO
+	src/t_mmap_dio -b $SCRATCH_MNT/tf_{s,d} \
+		$1 "buffered both dax"
+}
+
+t_nondax_to_dax()
+{
+	prep_files
+	$XFS_IO_PROG -c "chattr -x" $SCRATCH_MNT/tf_s
+	$XFS_IO_PROG -c "chattr +x" $SCRATCH_MNT/tf_d
+	src/t_mmap_dio $SCRATCH_MNT/tf_{s,d} \
+		$1 "dio nondax to dax"
+
+	prep_files
+	$XFS_IO_PROG -c "chattr -x" $SCRATCH_MNT/tf_s
+	$XFS_IO_PROG -c "chattr +x" $SCRATCH_MNT/tf_d
+	src/t_mmap_dio -b $SCRATCH_MNT/tf_{s,d} \
+		$1 "buffered nondax to dax"
+}
+
+t_dax_to_nondax()
+{
+	prep_files
+	$XFS_IO_PROG -c "chattr +x" $SCRATCH_MNT/tf_s
+	$XFS_IO_PROG -c "chattr -x" $SCRATCH_MNT/tf_d
+	src/t_mmap_dio $SCRATCH_MNT/tf_{s,d} \
+		$1 "dio dax to nondax"
+
+	prep_files
+	$XFS_IO_PROG -c "chattr +x" $SCRATCH_MNT/tf_s
+	$XFS_IO_PROG -c "chattr -x" $SCRATCH_MNT/tf_d
+	src/t_mmap_dio -b $SCRATCH_MNT/tf_{s,d} \
+		$1 "buffered dax to nondax"
+}
+
+t_both_nondax()
+{
+	prep_files
+	$XFS_IO_PROG -c "chattr -x" $SCRATCH_MNT/tf_{s,d}
+	src/t_mmap_dio $SCRATCH_MNT/tf_{s,d} \
+		$1 "dio both nondax"
+
+	prep_files
+	$XFS_IO_PROG -c "chattr -x" $SCRATCH_MNT/tf_{s,d}
+	src/t_mmap_dio -b $SCRATCH_MNT/tf_{s,d} \
+		$1 "buffered both nondax"
+}
+
+# $1 mmap read/write size
+t_dax_flag_mmap_dio()
+{
+	t_both_dax $1
+	t_dax_to_nondax $1
+	t_nondax_to_dax $1
+	t_both_nondax $1
+}
+
+do_tests()
+{
+	# less than page size
+	t_dax_flag_mmap_dio 1024
+	# page size
+	t_dax_flag_mmap_dio `src/feature -s`
+	# bigger sizes, for PMD faults
+	t_dax_flag_mmap_dio $((16 * 1024 * 1024))
+	t_dax_flag_mmap_dio $((64 * 1024 * 1024))
+}
+
+# make xfs 2Mb aligned for PMD fault testing
+_scratch_mkfs "-d su=2m,sw=1" > /dev/null 2>&1
+
+# mount with dax option
+_scratch_mount "-o dax"
+
+tsize=$((128 * 1024 * 1024))
+
+do_tests
+_scratch_unmount
+
+# mount again without dax option
+export MOUNT_OPTIONS=""
+_scratch_mount
+do_tests
+
+# success, all done
+echo "Silence is golden"
+status=0
+exit

tests/xfs/260.out
@@ -1,0 +1,2 @@
+QA output created by 260
+Silence is golden

tests/xfs/group
@@ -257,6 +257,7 @@
 257 auto quick clone
 258 auto quick clone
 259 auto quick
+260 auto attr quick
 261 auto quick quota
 263 auto quick quota
 264 auto quick mount

commit 47a3f8a8308ef62205a7014f0d48dc8f9cdf10e5

src/t_mmap_dio: fix incorrect argument count check
t_mmap_dio.c actually requires 4 arguments, not 3 as the current
check enforces:

usage: t_mmap_dio <src file> <dest file> <size> <msg>
open src(No such file or directory) len 0 (null)

Signed-off-by: Ross Zwisler <ross.zwisler@linux.intel.com>
Fixes: 456581661b4d ("xfs: test per-inode DAX flag by IO")
Reviewed-by: Eryu Guan <eguan@redhat.com>
Signed-off-by: Eryu Guan <eguan@redhat.com>

src/t_mmap_dio.c
@@ -39,7 +39,7 @@ int main(int argc, char **argv)
 	char *dfile;
 	unsigned long len, opt;
 
-	if (argc < 4)
+	if (argc < 5)
 		usage(basename(argv[0]));
 
 	while ((opt = getopt(argc, argv, "b")) != -1)