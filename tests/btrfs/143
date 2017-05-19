torvalds/linux

commit 20a7db8ab3f2057a518448b1728d504ffadef65e

btrfs: add dummy callback for readpage_io_failed and drop checks
Make extent_io_ops::readpage_io_failed_hook callback mandatory and
define a dummy function for btrfs_extent_io_ops. As the failed IO
callback is not performance critical, the branch vs extra trade off does
not hurt.

Signed-off-by: David Sterba <dsterba@suse.com>

fs/btrfs/disk-io.c
@@ -4658,7 +4658,7 @@ static const struct extent_io_ops btree_extent_io_ops = {
 	.readpage_end_io_hook = btree_readpage_end_io_hook,
 	/* note we're sharing with inode.c for the merge bio hook */
 	.merge_bio_hook = btrfs_merge_bio_hook,
+	.readpage_io_failed_hook = btree_io_failed_hook,
 
 	/* optional callbacks */
-	.readpage_io_failed_hook = btree_io_failed_hook,
 };

fs/btrfs/extent_io.c
@@ -2581,7 +2581,7 @@ static void end_bio_extent_readpage(struct bio *bio)
 		if (likely(uptodate))
 			goto readpage_ok;
 
-		if (tree->ops && tree->ops->readpage_io_failed_hook) {
+		if (tree->ops) {
 			ret = tree->ops->readpage_io_failed_hook(page, mirror);
 			if (!ret && !bio->bi_error)
 				uptodate = 1;

fs/btrfs/extent_io.h
@@ -103,6 +103,7 @@ struct extent_io_ops {
 	int (*merge_bio_hook)(struct page *page, unsigned long offset,
 			      size_t size, struct bio *bio,
 			      unsigned long bio_flags);
+	int (*readpage_io_failed_hook)(struct page *page, int failed_mirror);
 
 	/*
 	 * Optional hooks, called if the pointer is not NULL
@@ -110,7 +111,6 @@ struct extent_io_ops {
 	int (*fill_delalloc)(struct inode *inode, struct page *locked_page,
 			     u64 start, u64 end, int *page_started,
 			     unsigned long *nr_written);
-	int (*readpage_io_failed_hook)(struct page *page, int failed_mirror);
 
 	int (*writepage_start_hook)(struct page *page, u64 start, u64 end);
 	void (*writepage_end_io_hook)(struct page *page, u64 start, u64 end,

fs/btrfs/inode.c
@@ -10509,6 +10509,12 @@ out_inode:
 
 }
 
+__attribute__((const))
+static int dummy_readpage_io_failed_hook(struct page *page, int failed_mirror)
+{
+	return 0;
+}
+
 static const struct inode_operations btrfs_dir_inode_operations = {
 	.getattr	= btrfs_getattr,
 	.lookup		= btrfs_lookup,
@@ -10551,6 +10557,7 @@ static const struct extent_io_ops btrfs_extent_io_ops = {
 	.submit_bio_hook = btrfs_submit_bio_hook,
 	.readpage_end_io_hook = btrfs_readpage_end_io_hook,
 	.merge_bio_hook = btrfs_merge_bio_hook,
+	.readpage_io_failed_hook = dummy_readpage_io_failed_hook,
 
 	/* optional callbacks */
 	.fill_delalloc = run_delalloc_range,

kdave/xfstests

commit aaf96798d84a5554a92031fae386e7d8d5decce8

btrfs: regression test for nocsum buffered read's repair
This is to test whether buffered read retry-repair code is able to
work in raid1 case as expected.

Please note that without checksum, btrfs doesn't know if the data
used to repair is correct, so repair is more of resync which makes
sure that both of the copy has the same content.

Commit 20a7db8ab3f2 ("btrfs: add dummy callback for
readpage_io_failed and drop checks") introduced the regression.

The upstream fix is commit 9d0d1c8b1c9d ("Btrfs: bring back repair
during read")

Signed-off-by: Liu Bo <bo.li.liu@oracle.com>
Reviewed-by: Eryu Guan <eguan@redhat.com>
Reviewed-by: Filipe Manana <fdmanana@gmail.com>
Signed-off-by: Eryu Guan <eguan@redhat.com>

tests/btrfs/143
@@ -1,0 +1,150 @@
+#! /bin/bash
+# FS QA Test 143
+#
+# Regression test for btrfs buffered read's repair during read without checksum.
+#
+# This is to test whether buffered read retry-repair code is able to work in
+# raid1 case as expected.
+#
+# Please note that without checksum, btrfs doesn't know if the data used to
+# repair is correct, so repair is more of resync which makes sure that both
+# of the copy has the same content.
+#
+# Commit 20a7db8ab3f2 ("btrfs: add dummy callback for readpage_io_failed and drop
+# checks") introduced the regression.
+#
+# The upstream fix is
+#	commit 9d0d1c8b1c9d ("Btrfs: bring back repair during read")
+#
+#-----------------------------------------------------------------------
+# Copyright (c) 2017 Liu Bo.  All Rights Reserved.
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License as
+# published by the Free Software Foundation.
+#
+# This program is distributed in the hope that it would be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write the Free Software Foundation,
+# Inc.,  51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+#-----------------------------------------------------------------------
+#
+
+seq=`basename $0`
+seqres=$RESULT_DIR/$seq
+echo "QA output created by $seq"
+
+here=`pwd`
+tmp=/tmp/$$
+status=1	# failure is the default!
+trap "_cleanup; exit \$status" 0 1 2 3 15
+
+_cleanup()
+{
+	cd /
+	rm -f $tmp.*
+}
+
+# get standard environment, filters and checks
+. ./common/rc
+. ./common/filter
+
+# remove previous $seqres.full before test
+rm -f $seqres.full
+
+# real QA test starts here
+
+# Modify as appropriate.
+_supported_fs btrfs
+_supported_os Linux
+_require_fail_make_request
+_require_scratch_dev_pool 2
+
+_require_btrfs_command inspect-internal dump-tree
+_require_command "$FILEFRAG_PROG" filefrag
+
+get_physical()
+{
+        # $1 is logical address
+        # print chunk tree and find devid 2 which is $SCRATCH_DEV
+        $BTRFS_UTIL_PROG inspect-internal dump-tree -t 3 $SCRATCH_DEV | \
+	grep $1 -A 6 | awk '($1 ~ /stripe/ && $3 ~ /devid/ && $4 ~ /1/) { print $6 }'
+}
+
+SYSFS_BDEV=`_sysfs_dev $SCRATCH_DEV`
+
+start_fail()
+{
+	echo 100 > $DEBUGFS_MNT/fail_make_request/probability
+	# the 1st one fails the first bio which is reading 4k (or more due to
+	# readahead), and the 2nd one fails the retry of validation so that it
+	# triggers read-repair
+	echo 2 > $DEBUGFS_MNT/fail_make_request/times
+	echo 0 > $DEBUGFS_MNT/fail_make_request/verbose
+	echo 1 > $SYSFS_BDEV/make-it-fail
+}
+
+stop_fail()
+{
+	echo 0 > $DEBUGFS_MNT/fail_make_request/probability
+	echo 0 > $DEBUGFS_MNT/fail_make_request/times
+	echo 0 > $SYSFS_BDEV/make-it-fail
+}
+
+_scratch_dev_pool_get 2
+# step 1, create a raid1 btrfs which contains one 128k file.
+echo "step 1......mkfs.btrfs" >>$seqres.full
+
+mkfs_opts="-d raid1 -b 1G"
+_scratch_pool_mkfs $mkfs_opts >>$seqres.full 2>&1
+
+# -o nospace_cache makes sure data is written to the start position of the data
+# chunk
+_scratch_mount -o nospace_cache,nodatasum
+
+$XFS_IO_PROG -f -d -c "pwrite -S 0xaa -b 128K 0 128K" "$SCRATCH_MNT/foobar" | _filter_xfs_io
+
+# step 2, corrupt the first 64k of one copy (on SCRATCH_DEV which is the first
+# one in $SCRATCH_DEV_POOL
+echo "step 2......corrupt file extent" >>$seqres.full
+
+${FILEFRAG_PROG} -v $SCRATCH_MNT/foobar >> $seqres.full
+logical_in_btrfs=`${FILEFRAG_PROG} -v $SCRATCH_MNT/foobar | _filter_filefrag | cut -d '#' -f 1`
+physical_on_scratch=`get_physical ${logical_in_btrfs}`
+
+_scratch_unmount
+$XFS_IO_PROG -d -c "pwrite -S 0xbb -b 64K $physical_on_scratch 64K" $SCRATCH_DEV | _filter_xfs_io
+
+_scratch_mount -o nospace_cache
+
+# step 3, 128k buffered read (this read can repair bad copy)
+echo "step 3......repair the bad copy" >>$seqres.full
+
+# since raid1 consists of two copies, and the bad copy was put on stripe #1
+# while the good copy lies on stripe #0, the bad copy only gets access when the
+# reader's pid % 2 == 1 is true
+while true; do
+	# start_fail only fails the following buffered read so the repair is
+	# supposed to work.
+	echo 3 > /proc/sys/vm/drop_caches
+	start_fail
+	$XFS_IO_PROG -c "pread 0 4K" "$SCRATCH_MNT/foobar" > /dev/null &
+	pid=$!
+	wait
+	stop_fail
+	[ $((pid % 2)) == 1 ] && break
+done
+
+_scratch_unmount
+
+# check if the repair works
+$XFS_IO_PROG -c "pread -v -b 512 $physical_on_scratch 512" $SCRATCH_DEV | _filter_xfs_io
+
+_scratch_dev_pool_put
+# success, all done
+status=0
+exit

tests/btrfs/143.out
@@ -1,0 +1,39 @@
+QA output created by 143
+wrote 131072/131072 bytes at offset 0
+XXX Bytes, X ops; XX:XX:XX.X (XXX YYY/sec and XXX ops/sec)
+wrote 65536/65536 bytes at offset 136708096
+XXX Bytes, X ops; XX:XX:XX.X (XXX YYY/sec and XXX ops/sec)
+08260000:  aa aa aa aa aa aa aa aa aa aa aa aa aa aa aa aa  ................
+08260010:  aa aa aa aa aa aa aa aa aa aa aa aa aa aa aa aa  ................
+08260020:  aa aa aa aa aa aa aa aa aa aa aa aa aa aa aa aa  ................
+08260030:  aa aa aa aa aa aa aa aa aa aa aa aa aa aa aa aa  ................
+08260040:  aa aa aa aa aa aa aa aa aa aa aa aa aa aa aa aa  ................
+08260050:  aa aa aa aa aa aa aa aa aa aa aa aa aa aa aa aa  ................
+08260060:  aa aa aa aa aa aa aa aa aa aa aa aa aa aa aa aa  ................
+08260070:  aa aa aa aa aa aa aa aa aa aa aa aa aa aa aa aa  ................
+08260080:  aa aa aa aa aa aa aa aa aa aa aa aa aa aa aa aa  ................
+08260090:  aa aa aa aa aa aa aa aa aa aa aa aa aa aa aa aa  ................
+082600a0:  aa aa aa aa aa aa aa aa aa aa aa aa aa aa aa aa  ................
+082600b0:  aa aa aa aa aa aa aa aa aa aa aa aa aa aa aa aa  ................
+082600c0:  aa aa aa aa aa aa aa aa aa aa aa aa aa aa aa aa  ................
+082600d0:  aa aa aa aa aa aa aa aa aa aa aa aa aa aa aa aa  ................
+082600e0:  aa aa aa aa aa aa aa aa aa aa aa aa aa aa aa aa  ................
+082600f0:  aa aa aa aa aa aa aa aa aa aa aa aa aa aa aa aa  ................
+08260100:  aa aa aa aa aa aa aa aa aa aa aa aa aa aa aa aa  ................
+08260110:  aa aa aa aa aa aa aa aa aa aa aa aa aa aa aa aa  ................
+08260120:  aa aa aa aa aa aa aa aa aa aa aa aa aa aa aa aa  ................
+08260130:  aa aa aa aa aa aa aa aa aa aa aa aa aa aa aa aa  ................
+08260140:  aa aa aa aa aa aa aa aa aa aa aa aa aa aa aa aa  ................
+08260150:  aa aa aa aa aa aa aa aa aa aa aa aa aa aa aa aa  ................
+08260160:  aa aa aa aa aa aa aa aa aa aa aa aa aa aa aa aa  ................
+08260170:  aa aa aa aa aa aa aa aa aa aa aa aa aa aa aa aa  ................
+08260180:  aa aa aa aa aa aa aa aa aa aa aa aa aa aa aa aa  ................
+08260190:  aa aa aa aa aa aa aa aa aa aa aa aa aa aa aa aa  ................
+082601a0:  aa aa aa aa aa aa aa aa aa aa aa aa aa aa aa aa  ................
+082601b0:  aa aa aa aa aa aa aa aa aa aa aa aa aa aa aa aa  ................
+082601c0:  aa aa aa aa aa aa aa aa aa aa aa aa aa aa aa aa  ................
+082601d0:  aa aa aa aa aa aa aa aa aa aa aa aa aa aa aa aa  ................
+082601e0:  aa aa aa aa aa aa aa aa aa aa aa aa aa aa aa aa  ................
+082601f0:  aa aa aa aa aa aa aa aa aa aa aa aa aa aa aa aa  ................
+read 512/512 bytes at offset 136708096
+XXX Bytes, X ops; XX:XX:XX.X (XXX YYY/sec and XXX ops/sec)

tests/btrfs/group
@@ -144,3 +144,4 @@
 140 auto quick
 141 auto quick
 142 auto quick
+143 auto quick