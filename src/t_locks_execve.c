7bc26faa6b52a2035f659c90a2833a453a18bace
Xiong Zhou <xzhou@redhat.com>
2018-04-23 10:42:48 +0800

    generic: test record locks across execve in multithread process
    POSIX requires that record locks are preserved across an execve(2).
    But currently the locks are released if process is multithreaded at
    the time that execve is called.
    
    As Jeff Layton wrote in his patch:
    "
    In that case, we'll end up unsharing the files_struct but the locks
    will still have their fl_owner set to the address of the old one.
    Eventually, when the other threads die and the last reference to the
    old files_struct is put, any POSIX locks get torn down since it
    looks like a close occurred on them.
    
    The result is that all of your open files will be intact with none
    of the locks you held before execve.
    "
    
    Add a new regression test for this particular case.
    
    [Eryu: rewrite commit log and test description]
    
    Signed-off-by: Xiong Zhou <xzhou@redhat.com>
    Reviewed-by: Eryu Guan <guaneryu@gmail.com>
    Signed-off-by: Eryu Guan <guaneryu@gmail.com>

---
+++ src/t_locks_execve.c
@@ -0,0 +1,77 @@
+#ifndef _GNU_SOURCE
+#define _GNU_SOURCE
+#endif
+#include <stdio.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <string.h>
+#include <errno.h>
+#include <pthread.h>
+#include <unistd.h>
+#include <sys/wait.h>
+
+static void err_exit(char *op, int errn)
+{
+	fprintf(stderr, "%s: %s\n", op, strerror(errn));
+	exit(errn);
+}
+
+void *thread_fn(void *arg)
+{
+	/* execve will release threads */
+	while(1) sleep(1);
+	return NULL;
+}
+
+struct flock fl = {
+	.l_type = F_WRLCK,
+	.l_whence = SEEK_SET,
+	.l_start = 0,
+	.l_len = 1,
+};
+
+static void checklock(int fd)
+{
+	if (fcntl(fd, F_GETLK, &fl) < 0)
+		err_exit("getlk", errno);
+	if (fl.l_type == F_UNLCK) {
+		printf("record lock is not preserved across execve(2)\n");
+		exit(1);
+	}
+	exit(0);
+}
+
+int main(int argc, char **argv)
+{
+	int fd, flags;
+	char fdstr[10];
+	char *newargv[] = { argv[0], argv[1], fdstr, NULL };
+	pthread_t th;
+
+	/* passing fd in argv[2] in execve */
+	if (argc == 3) {
+		fd = atoi(argv[2]);
+		checklock(fd);
+		exit(0);
+	}
+
+	fd = open(argv[1], O_WRONLY|O_CREAT, 0755);
+	if (fd < 0)
+		err_exit("open", errno);
+	if (fcntl(fd, F_SETLK, &fl) < 0)
+		err_exit("setlk", errno);
+
+	/* require multithread process to reproduce the issue */
+	pthread_create(&th, NULL, thread_fn, &fd);
+
+	if ((flags = fcntl(fd, F_GETFD)) < 0)
+		err_exit("getfd", errno);
+	flags &= ~FD_CLOEXEC;
+	if (fcntl(fd, F_SETFD, flags) < 0)
+		err_exit("setfd", errno);
+
+	snprintf(fdstr, sizeof(fdstr), "%d", fd);
+	execve(argv[0], newargv, NULL);
+
+	return 0;
+}
