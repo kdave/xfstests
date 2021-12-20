92b9c0dedace427e5b4d176113f13c83499855cd
Theodore Ts'o <tytso@mit.edu>
2021-12-20 15:40:59 -0500

    ext4/033: test EXT4_IOC_RESIZE_FS by calling the ioctl directly
    E2fsprogs commits 4ea80d031c7e ("resize2fs: adjust new size of the
    file system to allow a successful resize") and 50088b1996cc
    ("resize2fs: attempt to keep the # of inodes valid by removing the
    last bg") will automatically reduce the requested new size of the file
    system by up to a single block group to avoid overflowing the 32-bit
    inode count.   This interferes with ext4/033's test of kernel commit
    4f2f76f75143 ("ext4: Forbid overflowing inode count when # resizing".)
    
    Address this by creating a new test program, ext4_resize which calls
    the EXT4_IOC_RESIZE_FS ioctl directly so we can correctly test the
    kernel's online resize code.
    
    Reported-by: Eric Whitney <enwlinux@gmail.com>
    Signed-off-by: Theodore Ts'o <tytso@mit.edu>
    Reviewed-by: Eryu Guan <guaneryu@gmail.com>
    Signed-off-by: Eryu Guan <guaneryu@gmail.com>

---
+++ src/ext4_resize.c
@@ -0,0 +1,50 @@
+// SPDX-License-Identifier: GPL-2.0
+
+/*
+ * Test program which uses the raw ext4 resize_fs ioctl directly.
+ */
+
+#include <stdio.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <unistd.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <sys/ioctl.h>
+#include <sys/mount.h>
+
+typedef unsigned long long __u64;
+
+#ifndef EXT4_IOC_RESIZE_FS
+#define EXT4_IOC_RESIZE_FS		_IOW('f', 16, __u64)
+#endif
+
+int main(int argc, char **argv)
+{
+	__u64	new_size;
+	int	error, fd;
+	char	*tmp = NULL;
+
+	if (argc != 3) {
+		fputs("insufficient arguments\n", stderr);
+		return 1;
+	}
+	fd = open(argv[1], O_RDONLY);
+	if (!fd) {
+		perror(argv[1]);
+		return 1;
+	}
+
+	new_size = strtoull(argv[2], &tmp, 10);
+	if ((errno) || (*tmp != '\0')) {
+		fprintf(stderr, "%s: invalid new size\n", argv[0]);
+		return 1;
+	}
+
+	error = ioctl(fd, EXT4_IOC_RESIZE_FS, &new_size);
+	if (error < 0) {
+		perror("EXT4_IOC_RESIZE_FS");
+		return 1;
+	}
+	return 0;
+}
