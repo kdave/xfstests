3bc939bccca554980b999b25720648ab211cc21b
Christian Brauner <brauner@kernel.org>
2022-05-12 18:52:48 +0200

    vfstest: split out btrfs idmapped mounts test
    Split the btrfs specific idmapped mounts test into a separate source
    file.
    
    Acked-by: Christoph Hellwig <hch@lst.de>
    Signed-off-by: Christian Brauner (Microsoft) <brauner@kernel.org>
    Signed-off-by: Zorro Lang <zlang@kernel.org>

---
+++ src/vfs/btrfs-idmapped-mounts.h
@@ -0,0 +1,15 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+
+#ifndef __BTRFS_IDMAPPED_MOUNTS_H
+#define __BTRFS_IDMAPPED_MOUNTS_H
+
+#ifndef _GNU_SOURCE
+#define _GNU_SOURCE
+#endif
+
+#include "utils.h"
+
+extern const struct test_suite s_btrfs_idmapped_mounts;
+
+#endif /* __BTRFS_IDMAPPED_MOUNTS_H */
+
